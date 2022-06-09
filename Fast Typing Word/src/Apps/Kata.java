a27ddd9bb5700814c6154182ecf19499220e16f0
andikariskys <andikarisky313@gmail.com>
2022-06-09 22:13:14 +0700

    first commit

---
+++ Fast Typing Word/src/Apps/Kata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Apps;
+
+/**
+ * 
+ * @author Andika Risky Septiawan
+ */
+public class Kata {
+    String[] indo4kata = {"kamu", "kata", "malu", "asam", "biru", "suka", "maaf", "abon", 
+        "adil", "baik", "cair", "ojek"};
+    String[] indo5kata = {"memar", "marah", "sakit", "sakti", "pergi", "susah", "acara", 
+        "agama", "bagus", "cabai", "capek", "akses"};
+    String[] indo6kata = {"dendam", "sayang", "terima", "ikhlas", "meniti", "sepeda", 
+        "bagasi", "bahari", "cabang", "elpiji", "empati", "hadiah"};
+    String[] indo7kata = {"perduli", "kembali", "harapan", "mencari", "pingsan", "lupakan", 
+        "agaknya", "adiksi", "tradisi", "bangkai", "elastis", "hiburan", "firasat"};
+    
+    String[] eng4kata = {"boat", "born", "zoom", "walk", "wall", "talk", "chat", "help", 
+        "what", "hope", "move", "slow"};
+    String[] eng5kata = {"chalk", "chair", "table", "jeans", "paint", "taste", "fight", 
+        "mouse", "paste", "color", "empty"};
+    String[] eng6kata = {"absurd", "accept", "safety", "jersey", "bubble", "bucket", 
+        "wallet", "asking", "versus", "runner"};
+    String[] eng7kata = {"scorpio", "jogging", "package", "waiting", "cartoon", "candied", 
+        "timming", "walking", "fastest"};
+}
